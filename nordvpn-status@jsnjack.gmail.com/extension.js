773b126101d2bc57894727560e23b2cd996c8b4c
Yauhen Shulitski <jsnjack@gmail.com>
2022-11-20 22:00:11 +0100

    Fix for gnome 43

--- nordvpn-status@jsnjack.gmail.com/extension.js
+++ nordvpn-status@jsnjack.gmail.com/extension.js
@@ -1,36 +1,37 @@
 const Mainloop = imports.mainloop;
-const Soup = imports.gi.Soup;
-const St = imports.gi.St;
 const Main = imports.ui.main;
-
+const {Soup, GLib, St} = imports.gi;
 
 let timeout;
 let indicator;
 const period = 5000;
+// https://gjs-docs.gnome.org/soup30~3.2.2/soup.message#constructor-new_from_encoded_form
 
 // Use log(data) to log
 function checkStatus() {
-    const options = {
-        timeout: 1,
-    };
-    const session = new Soup.Session(options);
-    const url = "https://nordvpn.com/wp-admin/admin-ajax.php";
-    const params = {
-        "action": "get_user_info_data",
-    };
-    const message = Soup.form_request_new_from_hash("GET", url, params);
-    session.queue_message(message, function(session, message) {
-        if (message.status_code === 200) {
-            const data = JSON.parse(message.response_body.data);
-            if (data.status) {
-                // log(data.status);
-                indicator.set_label("vpn");
-                indicator.set_style("color: #2DDE0D;");
-                return;
+    const session = new Soup.Session();
+    session.set_timeout(4);
+    const url = "https://nordvpn.com/wp-admin/admin-ajax.php?action=get_user_info_data";
+    const message = Soup.Message.new("GET", url);
+    session.send_and_read_async(
+        message,
+        GLib.PRIORITY_DEFAULT,
+        null,
+        (session, result) => {
+            if (message.get_status() === Soup.Status.OK) {
+                let bytes = session.send_and_read_finish(result);
+                let decoder = new TextDecoder("utf-8");
+                let response = decoder.decode(bytes.get_data());
+                const data = JSON.parse(response);
+                if (data.status) {
+                    indicator.set_label("vpn");
+                    indicator.set_style("color: #2DDE0D;");
+                    return;
+                }
             }
+            indicator.set_label("");
         }
-        indicator.set_label("");
-    });
+    );
 }
 
 // eslint-disable-next-line no-unused-vars
